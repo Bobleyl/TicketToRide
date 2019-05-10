553d761bc270f0eca17a6c8975726499d109806b
Bo Bleyl <boaustinbleyl@gmail.com>
2019-05-10 17:14:20 -0600

    Colors enum added

---
+++ app/src/main/java/androidteam/cs340/tickettoride/Shared/Colors.java
@@ -0,0 +1,10 @@
+package androidteam.cs340.tickettoride.Shared;
+
+public enum Colors {
+    green("green"), yellow("yellow"), red("red"), blue("blue"), black("black");
+
+    private String color;
+    private Colors(String color){
+        this.color = color;
+    }
+}
