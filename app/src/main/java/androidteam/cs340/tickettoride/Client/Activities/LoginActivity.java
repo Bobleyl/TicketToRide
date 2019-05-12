ccddab550226cb7e7905e7183c06b533a3686e7f
Seth Barton <sethwbarton@gmail.com>
2019-05-11 19:40:12 -0600

    Added grayed out buttons till all fields are filled out. Set onClickListeners for buttons which display toasts on click.

--- app/src/main/java/androidteam/cs340/tickettoride/Client/Activities/LoginActivity.java
+++ app/src/main/java/androidteam/cs340/tickettoride/Client/Activities/LoginActivity.java
@@ -2,17 +2,74 @@ package androidteam.cs340.tickettoride.Client.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidteam.cs340.tickettoride.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText mUsernameField;
+    private EditText mPasswordField;
+    private Button mLoginButton;
+    private Button mRegisterButton;
+
+    private StringBuilder username;
+    private StringBuilder password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
+
+        mUsernameField = (EditText) findViewById(R.id.userNameField);
+        mUsernameField.addTextChangedListener(loginTextWatcher);
+
+        mPasswordField = (EditText) findViewById(R.id.passwordField);
+        mPasswordField.addTextChangedListener(loginTextWatcher);
+
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+        mRegisterButton = (Button) findViewById(R.id.registerButton);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create Command here to send to LoginTask?
+                Toast.makeText(getBaseContext(),"Logging you in...",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create Command here to send to RegisterTask?
+                Toast.makeText(getBaseContext(),"Registering...",Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
+
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            username =  new StringBuilder(mUsernameField.getText());
+            password = new StringBuilder(mPasswordField.getText());
+
+            mLoginButton.setEnabled(!username.toString().isEmpty() && !password.toString().isEmpty());
+            mRegisterButton.setEnabled(!username.toString().isEmpty() && !password.toString().isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {}
+    };
+
+
 }
