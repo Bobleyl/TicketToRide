3289bdbba2a297819c98ba994aaa86c8df799f28
Seth Barton <sethwbarton@gmail.com>
2019-05-11 19:00:04 -0600

    Deleted gson-2.6.2.jar from /libs as I got a "duplicate class" error when compiling. Created LoginActivity in replace of MainActivity and it's necessary string values and xml.

--- app/src/main/java/androidteam/cs340/tickettoride/Client/Activities/LoginActivity.java
+++ app/src/main/java/androidteam/cs340/tickettoride/Client/Activities/LoginActivity.java
@@ -1,15 +1,18 @@
-package androidteam.cs340.tickettoride.Client;
+package androidteam.cs340.tickettoride.Client.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import androidteam.cs340.tickettoride.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_login);
+
+
     }
 }
