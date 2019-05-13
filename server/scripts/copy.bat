5756ea3cf39f6d3d5f8cbe2d978ebe26228c1c36
Tyler <tacallaway@gmail.com>
2019-05-12 21:31:43 -0600

    Rename files

--- server/scripts/copy.bat
+++
@@ -1,1 +0,0 @@
-scp -i ticket-to-ride.pem ../../out/artifacts/TicketToRide_jar/TicketToRide.jar ec2-user@ec2-18-224-234-208.us-east-2.compute.amazonaws.com:~
---
+++ server/scripts/scp.bat
@@ -0,0 +1,1 @@
+scp -i ticket-to-ride.pem ../../build/classes/artifacts/TicketToRide_jar/TicketToRide.jar ec2-user@ec2-18-224-234-208.us-east-2.compute.amazonaws.com:~
