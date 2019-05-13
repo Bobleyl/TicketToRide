049bddac53b50d28e938b2a23be7dd89ad318745
Bo Bleyl <boaustinbleyl@gmail.com>
2019-05-13 12:26:03 -0600

    final changes to player class

--- app/src/main/java/androidteam/cs340/tickettoride/Shared/Player.java
+++ app/src/main/java/androidteam/cs340/tickettoride/Shared/Player.java
@@ -1,22 +1,12 @@
 package androidteam.cs340.tickettoride.Shared;
 
 public class Player {
-    String gameUID;
-    String UID;
     String color;
     String name;
 
-    public Player(String name, String Color, String GameUID, String PlayerUID){
-        this.gameUID = GameUID;
+    public Player(String name, String Color){
         this.color = color;
         this.name = name;
-        this.UID = PlayerUID;
-    }
-
-    String getUID(){ return UID; }
-
-    String getGameUID(){
-        return gameUID;
     }
 
     String getName(){ return name; }
