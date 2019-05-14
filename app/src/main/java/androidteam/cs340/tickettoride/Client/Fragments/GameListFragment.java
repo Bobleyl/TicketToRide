b74b9437650403b86fd94ebf97683efa483c105c
Seth Barton <sethwbarton@gmail.com>
2019-05-13 21:51:26 -0600

    Added GameListFragment and associated layout files.

--- app/src/main/java/androidteam/cs340/tickettoride/Client/Fragments/GameListFragment.java
+++ app/src/main/java/androidteam/cs340/tickettoride/Client/Fragments/GameListFragment.java
@@ -1,7 +1,34 @@
 package androidteam.cs340.tickettoride.Client.Fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidteam.cs340.tickettoride.R;
 
 public class GameListFragment extends Fragment {
+    private RecyclerView mGameRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_game_list, container, false);
+
+        mGameRecyclerView = (RecyclerView) view
+                .findViewById(R.id.game_list_recycler_view);
+        mGameRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
+
+    private class GameHolder extends RecyclerView.ViewHolder {
+        public GameHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_game, parent, false));
+        }
+    }
 
 }
