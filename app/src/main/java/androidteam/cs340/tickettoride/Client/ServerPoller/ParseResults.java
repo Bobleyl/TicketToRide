995c56e3117a88fc4df40294ae3d133180c37155
bkleinman7 <bleinman7@gmail.com>
2019-05-15 12:35:56 -0600

    Added parse methods

--- app/src/main/java/androidteam/cs340/tickettoride/Client/ServerPoller/ParseResults.java
+++ app/src/main/java/androidteam/cs340/tickettoride/Client/ServerPoller/ParseResults.java
@@ -1,5 +1,20 @@
 package androidteam.cs340.tickettoride.Client.ServerPoller;
 
+import java.util.ArrayList;
+
+import androidteam.cs340.tickettoride.Shared.Game;
+import androidteam.cs340.tickettoride.Shared.Result;
+
 public class ParseResults {
+
+    public String parseSingleString(Result result) {
+        result.getData();
+        return "";
+    }
+
+    public ArrayList<Game> parseLobbyResult(Result result) {
+        ArrayList<Game> games = new ArrayList<Game>();
+        return games;
+    }
 
 }
