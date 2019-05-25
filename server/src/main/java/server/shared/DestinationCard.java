3e331b38f53b43a8653bc3eb17e7f8cf9c0c6413
Tyler <tacallaway@gmail.com>
2019-05-24 20:05:46 -0600

    Destination card implementation in game model

---
+++ server/src/main/java/server/shared/DestinationCard.java
@@ -0,0 +1,97 @@
+package server.shared;
+
+public enum DestinationCard {
+
+    Denver_ElPaso {
+        public int points = 4;
+    },
+    KansasCity_Houston {
+        public int points = 5;
+    },
+    NewYork_Atlanta {
+        public int points = 6;
+    },
+    Calgary_SaltLakeCity {
+        public int points = 7;
+    },
+    Chicago_NewOrleans {
+        public int points = 7;
+    },
+    Helena_LosAngeles {
+        public int points = 8;
+    },
+    Duluth_Houston {
+        public int points = 8;
+    },
+    SaultSteMarie_Nashville {
+        public int points = 8;
+    },
+    Montreal_Atlanta {
+        public int points = 9;
+    },
+    SaultSteMarie_OklahomaCity {
+        public int points = 9;
+    },
+    Seattle_LosAngeles {
+        public int points = 9;
+    },
+    Chicago_SantaFe {
+        public int points = 9;
+    },
+    Duluth_ElPaso {
+        public int points = 10;
+    },
+    Toronto_Miami {
+        public int points = 10;
+    },
+    Portland_Phoenix {
+        public int points = 11;
+    },
+    Dallas_NewYorkCity {
+        public int points = 11;
+    },
+    Denver_Pittsburgh {
+        public int points = 11;
+    },
+    Winnipeg_LittleRock {
+        public int points = 11;
+    },
+    Winnipeg_Houston {
+        public int points = 12;
+    },
+    Boston_Miami {
+        public int points = 12;
+    },
+    Vancouver_SantaFe {
+        public int points = 13;
+    },
+    Calgary_Phoenix {
+        public int points = 13;
+    },
+    Montreal_NewOrleans {
+        public int points = 13;
+    },
+    LosAngeles_Chicago {
+        public int points = 16;
+    },
+    SanFrancisco_Atlanta {
+        public int points = 17;
+    },
+    Portland_Nashville {
+        public int points = 17;
+    },
+    Vancouver_Montreal {
+        public int points = 20;
+    },
+    LosAngeles_Miami {
+        public int points = 20;
+    },
+    LosAngeles_NewYorkCity {
+        public int points = 21;
+    },
+    Seattle_NewYorkCity {
+        public int points = 22;
+    };
+
+    public int points = 0;
+}
