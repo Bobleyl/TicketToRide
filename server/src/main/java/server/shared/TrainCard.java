0b69f8e254c4065561913f697303559b889d8ff3
bkleinman7 <bleinman7@gmail.com>
2019-05-25 21:56:38 -0600

    updated enums...

--- server/src/main/java/server/shared/TrainCard.java
+++ server/src/main/java/server/shared/TrainCard.java
@@ -2,33 +2,31 @@ package server.shared;
 
 public enum TrainCard {
 
-    Locomotive {
-        public String color = "wild";
-    },
-    Box {
-        public String color = "pink";
-    },
-    Passenger {
-        public String color = "white";
-    },
-    Tanker {
-        public String color = "blue";
-    },
-    Reefer {
-        public String color = "yellow";
-    },
-    Freight {
-        public String color = "orange";
-    },
-    Hopper {
-        public String color = "black";
-    },
-    Coal {
-        public String color = "red";
-    },
-    Caboose {
-        public String color = "green";
-    };
+    Locomotive ("wild"),
+
+    Box ("pink"),
+
+    Passenger ("white"),
+
+    Tanker ("blue"),
+
+    Reefer ("yellow"),
+
+    Freight ("orange"),
+
+    Hopper ("black"),
+
+    Coal("red"),
+
+    Caboose("green");
 
     public String color = "";
+
+    TrainCard(String color_) {
+        color = color_;
+    }
+
+    String showColor() {
+        return color;
+    }
 }
