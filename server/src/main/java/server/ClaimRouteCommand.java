ed0e8fb204fc70d60253771502255d41319c8974
bkleinman7 <bleinman7@gmail.com>
2019-05-26 11:58:32 -0600

    updated claimRouteCommand

--- server/src/main/java/server/ClaimRouteCommand.java
+++ server/src/main/java/server/ClaimRouteCommand.java
@@ -34,6 +34,8 @@ public class ClaimRouteCommand implements CommandInterface {
                     if(player.getUID().equals(playerID)) {
 
                         List<TrainCard> trainCards = player.getTrainCardsHand();
+                        TrainCardDeck trainCardDeck = game.getTrainCardDeck();
+
                         //Make sure player has enough colors to claim route
                         int foundColors = 0;
 
@@ -41,7 +43,10 @@ public class ClaimRouteCommand implements CommandInterface {
                         System.out.println("Route Length: " + route.length);
 
                         for(TrainCard trainCard : trainCards) {
-                            System.out.println("Train: " + trainCard.color);
+                            System.out.println("Train: " + trainCard + ", color: " + trainCard.color);
+                            //Idea is if the route color is grey, they can use any cards..
+                            //If they have a wild that will increase the count
+                            //Or if they have a card that matches the route color
                             if(trainCard.color.equals(route.color) ||
                                     trainCard.color.equals(TrainCard.Locomotive.color) ||
                                     trainCard.color.equals("grey")) {
@@ -51,36 +56,60 @@ public class ClaimRouteCommand implements CommandInterface {
 
                         if (foundColors >= route.length) {
 
-                            player.claimRoute(route);
                             System.out.println("claimed route");
+                            player.claimRoute(route);
                             //Update points, length to point relation
                             //1:1, 2:2, 3:4, 4:7, 5:10, 6:15
                             int score = 0;
                             int length = route.length;
 
                             //Logic to increase player score
-                            if (length == 1) {
-                                score = 1;
+                            switch (length) {
+                                case 1:
+                                    score = 1;
+                                    break;
+                                case 2:
+                                    score = 2;
+                                    break;
+                                case 3:
+                                    score = 4;
+                                    break;
+                                case 4:
+                                    score = 7;
+                                    break;
+                                case 5:
+                                    score = 10;
+                                    break;
+                                case 6:
+                                    score = 15;
+                                    break;
                             }
-                            else if (length == 2) {
-                                score = 2;
-                            }
-                            else if (length == 3) {
-                                score = 4;
-                            }
-                            else if (length == 4) {
-                                score = 7;
-                            }
-                            else if (length == 5) {
-                                score = 10;
-                            }
-                            else if (length == 6) {
-                                score = 15;
-                            }
+
                             //Take original score and add it to new score
                             player.setScore(player.getScore()+score);
                             successfulExecute = true;
                             returnMessage.setReponseMessage("success");
+
+                            //Now remove cards from players deck
+                            int maxRemove = 0;
+                            for(int j = 0; j < trainCards.size(); j++) {
+                                if (trainCards.get(j).color.equals(route.color) ||
+                                        trainCards.get(j).color.equals(TrainCard.Locomotive.color) ||
+                                        route.color.equals("grey")) {
+                                    //System.out.println(trainCards.indexOf(trainCard));
+                                    trainCardDeck.returnCard(trainCards.get(j));
+                                    System.out.println("Removed: " + trainCards.get(j));
+                                    trainCards.remove(j);
+                                    //Don't remove more cards than the length of the route
+                                    maxRemove++;
+                                    if (maxRemove == route.length) {
+                                        break;
+                                    }
+                                }
+                            }
+
+                            //Update how many trainCars the player has
+                            player.setTrainCars(player.getTrainCars()-route.length);
 
                         } else {
                             System.out.println("Player doesn't have enough cards to claim route (" +
