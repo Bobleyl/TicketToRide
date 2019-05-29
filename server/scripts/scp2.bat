a238e1fe541ad16cee0aac8e89d8cdf25a846684
Tyler <tacallaway@gmail.com>
2019-05-28 22:43:52 -0600

    Update pom to make executable jar

--- server/scripts/scp2.bat
+++ server/scripts/scp2.bat
@@ -1,1 +1,1 @@
-scp -i ticket-to-ride.pem ../../build/classes/artifacts/TicketToRide_jar/TicketToRide.jar ec2-user@ec2-18-224-234-208.us-east-2.compute.amazonaws.com:~
+scp -i ticket-to-ride.pem ../target/TicketToRide-1.0-jar-with-dependencies.jar ec2-user@ec2-18-224-234-208.us-east-2.compute.amazonaws.com:~
