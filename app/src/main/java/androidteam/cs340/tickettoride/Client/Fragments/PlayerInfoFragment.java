f0da0bb099c02c9bcf398352b4bdf44414b1cc71
Bo Bleyl <boaustinbleyl@gmail.com>
2019-06-04 14:05:49 -0600

    commented out turn button logic till bug is fixed

--- app/src/main/java/androidteam/cs340/tickettoride/Client/Fragments/PlayerInfoFragment.java
+++ app/src/main/java/androidteam/cs340/tickettoride/Client/Fragments/PlayerInfoFragment.java
@@ -99,21 +99,21 @@ public class PlayerInfoFragment extends Fragment implements IPresenter {
         View view = inflater.inflate(R.layout.fragment_player_info, container, false);
 
         mDestinationDeck = view.findViewById(R.id.destinationDeck);
-        mDestinationDeck.setEnabled(isMyTurn());
+        //mDestinationDeck.setEnabled(isMyTurn());
         //mDestinationCards = view.findViewById(R.id.destinationCards);
         mWhoseTurn = view.findViewById(R.id.whoseTurn);
         mDownDeck = view.findViewById(R.id.downDeck);
-        mDownDeck.setEnabled(isMyTurn());
+        //mDownDeck.setEnabled(isMyTurn());
         mUpDeck1 = view.findViewById(R.id.upDeck1);
-        mUpDeck1.setEnabled(isMyTurn());
+        //mUpDeck1.setEnabled(isMyTurn());
         mUpDeck2 = view.findViewById(R.id.upDeck2);
-        mUpDeck2.setEnabled(isMyTurn());
+        //mUpDeck2.setEnabled(isMyTurn());
         mUpDeck3 = view.findViewById(R.id.upDeck3);
-        mUpDeck3.setEnabled(isMyTurn());
+        //mUpDeck3.setEnabled(isMyTurn());
         mUpDeck4 = view.findViewById(R.id.upDeck4);
-        mUpDeck4.setEnabled(isMyTurn());
+        //mUpDeck4.setEnabled(isMyTurn());
         mUpDeck5 = view.findViewById(R.id.upDeck5);
-        mUpDeck5.setEnabled(isMyTurn());
+        //mUpDeck5.setEnabled(isMyTurn());
         mOrangeCount = view.findViewById(R.id.orangeCount);
         mRedCount = view.findViewById(R.id.redCount);
         mWildCount = view.findViewById(R.id.wildCount);
