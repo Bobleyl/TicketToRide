cc70e9f5bcff34cb37fee4ae91612a1f3ebabed5
Bo Bleyl <boaustinbleyl@gmail.com>
2019-06-06 17:24:39 -0600

    .

--- app/src/main/java/androidteam/cs340/tickettoride/Client/Fragments/MessageFragment.java
+++ app/src/main/java/androidteam/cs340/tickettoride/Client/Fragments/MessageFragment.java
@@ -15,6 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.VideoView;
+import android.graphics.Color;
 
 import java.util.ArrayList;
 import java.util.Collections;
