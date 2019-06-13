9f221176d5b20b5d4be9fac471850657ef945920
Tyler <tacallaway@gmail.com>
2019-06-12 22:46:52 -0600

    Create PluginFactory in ServerCommunicator

--- server/src/main/java/server/ServerCommunicator.java
+++ server/src/main/java/server/ServerCommunicator.java
@@ -1,10 +1,15 @@
 package server;
 
+import Shared.IFactory;
 import io.javalin.Javalin;
 
 public class ServerCommunicator {
+    public static IFactory factory;
+
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
         app.post("/execute", new CommandHandler());
+
+        factory = AbstractFactory.createFactory(args[0]);
     }
 }
