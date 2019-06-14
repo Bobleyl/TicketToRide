015f5ce1c5946f084530bef21da0c0f4b36fae67
bkleinman7 <bleinman7@gmail.com>
2019-06-14 08:23:52 -0600

    updated DAOs

--- server/src/main/java/server/GameDAO.java
+++ server/src/main/java/server/GameDAO.java
@@ -1,9 +1,9 @@
 package server;
 
-import androidteam.cs340.tickettoride.Shared.GameModel;
 import com.google.gson.Gson;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameDAO {
@@ -12,23 +12,26 @@ public class GameDAO {
 
     public static GameDAO SINGLETON = new GameDAO();
 
-    public void storeDelta(String gameID, List<String> delta) {
+    public static void main(String[] args) {
+        List<String> testRun = new ArrayList<>();
+        //testRun.add("ok1");
+        GameDAO.SINGLETON.storeDelta("test", testRun);
+        GameDAO.SINGLETON.retrieveGames();
+        //GameDAO.SINGLETON.deleteGames();
+    }
+
+    public void deleteGame(String gameID) {
+
+        Connection connection = DataConnection.SINGLETON.connectJDBCToAWSEC2();
+        Statement statement;
 
         try {
 
-            //If it exists create it, if not update it..
-
-            Gson gson = new Gson();
-            String deltas = gson.toJson(delta);
-
-            Connection connection = DataConnection.SINGLETON.connectJDBCToAWSEC2();
-            Statement statement;
             statement = connection.createStatement();
-            String query = "UPDATE GAME SET DELTA = ? WHERE GAMEID = ?";
+            String query = "DELETE FROM GAME WHERE GAMEID = ?";
 
             try (PreparedStatement stmt = connection.prepareStatement(query)) {
-                stmt.setString(1, deltas);
-                stmt.setString(2, gameID);
+                stmt.setString(1, gameID);
                 stmt.execute();
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -42,11 +45,114 @@ public class GameDAO {
 
     }
 
-    public void storeGame(String gameID, GameModel gameModel) {
+    public void deleteGames() {
+
+        Connection connection = DataConnection.SINGLETON.connectJDBCToAWSEC2();
+        Statement statement;
 
         try {
 
-            //If it exists create it, if not update it..
+            statement = connection.createStatement();
+            String query = "DELETE FROM GAME";
+
+            try (PreparedStatement stmt = connection.prepareStatement(query)) {
+                stmt.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+    }
+
+    public void storeDelta(String gameID, List<String> delta) {
+
+        Connection connection = DataConnection.SINGLETON.connectJDBCToAWSEC2();
+        Statement statement;
+
+        boolean gameExists = true;
+        if (connection != null) {
+            try {
+
+                String querySearchGame = "SELECT * FROM GAME WHERE GAMEID = ?;";
+                try (PreparedStatement stmt1 = connection.prepareStatement(querySearchGame)) {
+                    stmt1.setString(1, gameID);
+                    try(ResultSet resultSet = stmt1.executeQuery()) {
+                        gameExists = resultSet.next();
+                    }
+                } catch(SQLException se) {
+                    se.printStackTrace();
+                    gameExists = false;
+                }
+
+                //If userExists = True then return false
+                System.out.println(gameExists);
+                if (gameExists) {
+
+                    try {
+
+                        //If it exists create it, if not update it..
+
+                        Gson gson = new Gson();
+                        String deltas = gson.toJson(delta);
+
+                        statement = connection.createStatement();
+                        String query = "UPDATE GAME SET DELTA = ? WHERE GAMEID = ?";
+
+                        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+                            stmt.setString(1, deltas);
+                            stmt.setString(2, gameID);
+                            stmt.execute();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+
+                        statement.close();
+                        connection.close();
+                    } catch (Exception e) {
+                        System.out.println(e.toString());
+                    }
+                } else {
+
+                    try {
+
+                        //If it exists create it, if not update it..
+
+                        Gson gson = new Gson();
+                        String deltas = gson.toJson(delta);
+
+                        statement = connection.createStatement();
+                        String query = "INSERT INTO GAME (GAMEID, DELTA) VALUES (?, ?);";
+
+                        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+                            stmt.setString(1, gameID);
+                            stmt.setString(2, gson.toJson(delta));
+                            stmt.execute();
+                        } catch (SQLException e) {
+                            System.out.println(e.toString());
+                        }
+
+                        statement.close();
+                        connection.close();
+                    } catch (Exception e) {
+                        System.out.println(e.toString());
+                    }
+
+                }
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+        }
+
+    }
+
+    public void storeGame(String gameID, GameModel gameModel) {
+
+        try {
 
             Gson gson = new Gson();
             String games = gson.toJson(gameModel);
@@ -84,7 +190,9 @@ public class GameDAO {
             ResultSet rs = statement.executeQuery(query);
 
             while (rs.next()) {
-                //Do some logic here to rebuild the game
+                System.out.println(rs.getString(1));
+                System.out.println(rs.getString(2));
+                System.out.println(rs.getString(3));
             }
 
             statement.close();
